ebae116139b792de0050e917c511ac37a58c74cf
tpsoete <tpsoete@163.com>
2018-10-30 22:24:27 +0800

    更新Record类

--- app/database/db.py
+++ app/database/db.py
@@ -1,4 +1,5 @@
 from sqlite3 import *
+from abc import abstractmethod
 import os
 
 from config import config
@@ -20,6 +21,12 @@ class Database:
         self.conn.close()
 
     def query(self, sql):
+        """
+        数据库查询，返回查询结果
+        :param sql: SQL 查询语句
+        :return: 查询结果（tuple 列表）
+        """
+
         cursor = self.conn.cursor()
         try:
             cursor.execute(sql)
@@ -44,6 +51,31 @@ class Database:
         return cursor.rowcount
 
 
+class DBNull:
+    """
+    用于 INSERT 语句中将 Python 的 None 转化为 null 输出
+    """
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'null'
+
+    def __repr__(self):
+        return 'null'
+
+    @staticmethod
+    def convert(tpl):
+        """将 tuple 中的 None 替换为 null"""
+        ret = []
+        for item in tpl:
+            if item is None:
+                ret.append(DBNull())
+            else:
+                ret.append(item)
+        return tuple(ret)
+
+
 class Record:
     """
     数据库记录（抽象类）
@@ -51,10 +83,11 @@ class Record:
 
     _db = 'test.db'    # 数据库文件
     _table = None      # 表名
-    _ddl = None        # 表定义（CREATE语句）
+    _ddl = None        # 表定义（CREATE 语句）
 
     @classmethod
     def tablename(cls):
+        """获得表名"""
         return cls._table
 
     @classmethod
@@ -66,9 +99,24 @@ class Record:
             return
 
         db = Database(cls._db)
-        db.modify('DROP TABLE IF EXISTS ' + cls._table)
+        db.modify("DROP TABLE IF EXISTS " + cls._table)
         db.modify(cls._ddl)
 
-    def __init__(self):
+    def __init__(self, tpl):
+        if type(tpl) != tuple:
+            raise TypeError('Record 类型构造需要 tuple 类型')
+
+    @abstractmethod
+    def to_tuple(self):
+        """转化为 tuple 用于 INSERT 语句"""
         pass
 
+    def insert(self, database=None):
+        """
+        将数据插入到对应表中
+        :param database: 指定数据库连接，不指定则新键
+        """
+        if database is not Database:
+            database = Database(self._db)
+        sql = "INSERT INTO %s VALUES %s" % (self._table, DBNull.convert(self.to_tuple()))
+        database.modify(sql)
