e2d26b9fed4d4ca0ca9be4cce54dc35404c20a49
tpsoete <tpsoete@163.com>
2018-10-31 12:49:27 +0800

    保留最后一次执行的语句。 更新__init__.py

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -3,11 +3,9 @@ from app.database.album import *
 from app.database.video import *
 from app.database.relation import *
 
-__all__ = ['User', 'Album', 'Video', 'Relation']
+__all__ = ['Database', 'Record',
+           'User', 'Album', 'Video', 'Relation']
 
 
 if __name__ == '__main__':
-    db = Database('test.db')
-    Relation.reset()
-    db.modify("insert into relation values('11','12','2')")
-    print(db.query('select * from relation'))
+    pass
