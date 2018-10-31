76b5f040101106f755a61373ffa9c9c6d8923d54
Gjiumian <44493544+Gjiumian@users.noreply.github.com>
2018-10-31 14:42:30 +0800

    ++

--- app/database/relation.py
+++ app/database/relation.py
@@ -10,9 +10,10 @@ class Relation(Record):
     _table = 'relation'
     _ddl = '''
     CREATE TABLE relation(
-        id1 VARCHAR(20) PRIMARY KEY,
+        id1 VARCHAR(20) NOT NULL,
         id2 VARCHAR(20) NOT NULL,
-        level int
+        level int,
+        PRIMARY KEY(id1,id2)
     );
     '''
 
@@ -26,6 +27,21 @@ class Relation(Record):
     def from_tuple(cls, tpl):
         self = cls()
         self.id1, self.id2, self.level = tpl
+        return self
 
     def to_tuple(self):
         return self.id1, self.id2, self.level
+
+    @classmethod
+    def get_level(cls, uid1, uid2):
+        db = cls.connect()
+        req = '''
+        SELECT level
+        FROM relation
+        WHERE id1=%s AND
+        id2=%s
+        ''' % (uid1, uid2)
+        level = db.query(req)
+        if len(level) == 0:
+            return None
+        return level[0][0]
