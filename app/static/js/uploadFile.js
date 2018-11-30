27eb92fbd08137dfeba33f260aab0a0ecaa0c11c
doylexxx <35215278+doylexxx@users.noreply.github.com>
2018-11-30 13:58:16 +0800

    添加网页2
    Signed-off-by: doylexxx <35215278+doylexxx@users.noreply.github.com>

---
+++ app/static/js/uploadFile.js
@@ -0,0 +1,20 @@
+$("#buttonAdd").on("change", function () {
+    var formData = new FormData($("#buttonAdd")[0]);
+    $.ajax({
+        url:"/upload",
+        type:"POST",
+        data:formData,
+        dataType:"json",
+        async:true,
+        cache:false,
+        contentType:false,
+        processData:false,
+        success:function (json_data) {
+            alert("upload succeed!")
+        },
+        error:function (json_data) {
+            alert(json_data)
+            console.log(json_data)
+        }
+    });
+});
